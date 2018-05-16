dcf4c3b125b6338c83040052fb14ab221dd03152
Vineet Jain <jain.vinee@gmail.com>
2018-05-16 09:18:44 -0700

    Add autocommit to make syncvscode

--- sync.mk
+++ sync.mk
@@ -3,3 +3,4 @@ syncvscode:
 	code --list-extensions > ./vscode/packages.txt
 	cp $(HOME)/Library/Application\ Support/Code/User/settings.json ./vscode/settings.json
 	cp $(HOME)/Library/Application\ Support/Code/User/keybindings.json ./vscode/keybindings.json
+	git commit -a -m "[Autocommit] Syncing VSCode settings"
