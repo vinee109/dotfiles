d251c8b824e0f507c8f33108eca0b33bed1252aa
Vineet Jain <jain.vinee@gmail.com>
2020-03-27 19:34:09 -0700

    [zsh] Adding zsh setup

---
+++ zsh/custom/aliases.zsh
@@ -0,0 +1,76 @@
+# ================================================================================
+# SYSTEM ALIASES
+#
+# Aliases for system commands like 'cd', 'ls', etc.
+# ================================================================================
+alias ..="cd .."
+alias cd="cdenv"
+alias cp="cp -i"
+alias df="df -H"
+alias gcc="gcc -Wall"
+alias ll="ls -l"
+alias ls="ls -AFG"
+alias mv="mv -i"
+alias rm="rm -i"
+alias vim="nvim"
+alias python="python2"
+
+# automating manual things
+alias cdown="mv ~/Downloads/* ~/.Trash"
+alias check="ping -o www.google.com"
+alias diskspace="du | sort -n -r | more"
+alias ipaddress="ifconfig | grep 'inet ' | grep -v 127.0.0.1 | tail -n 1 | sed 's/.*inet \([^ ]*\) .*/\1/'"
+alias sublime="open -a \"Sublime Text\""
+alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+alias preview="open -a \"Preview\""
+alias rmsc="find ~/Desktop -name \"Screen Shot*.png\" -delete"
+
+# ================================================================================
+# ARCANIST ALIIASES
+#
+# Aliases for common usages of arc
+# ================================================================================
+alias ad="arc diff"
+alias ado="arc diff --only"
+alias alt="arc lint"
+alias al="arc land"
+
+# ================================================================================
+# GIT
+#
+# Aliases and functions specific to git commands
+# ================================================================================
+
+alias ga="git add"
+alias gai="git add -i"
+alias gap="git add -p"
+alias gb="git branch"
+alias gbm="git branch -m"
+alias gc="git commit"
+alias gcam="git commit -a -m"
+alias gcamend="git commit --amend"
+alias gch="git checkout"
+alias gchb="git checkout -b"
+alias gchm="git checkout master"
+alias gcl="git clone"
+alias gcm="git commit -m"
+alias gd="git diff"
+alias gds="git diff --staged"
+alias gh="git help"
+alias ghash="git rev-parse"
+alias gg="git graph"
+alias gl="git log --pretty=format:'%C(yellow)%<(10)%h %Cgreen%<(14)%cr %C(bold magenta)%<(24)%aE %C(bold blue)%d %Creset %s' --abbrev-commit"
+alias gm="git merge"
+alias gps="git push"
+alias gpu="git pull"
+alias gr="git rm --cached"
+alias gri="git rebase -i"
+alias grim="git rebase -i master"
+alias grc="git rebase --continue"
+alias gs="git status"
+alias GS="gs"  # common typo
+alias gsp="git stash pop"
+alias gst="git stash"
+alias pull="git submodule update && git pull"
+alias undo="git reset HEAD~"
+
---
+++ zsh/custom/util.zsh
@@ -0,0 +1,74 @@
+# ================================================================================
+# UTILITIES
+#
+# Functions meant to simplify repetitive commands
+# ================================================================================
+
+# **************************************************************
+# Converts a Quicktime .mov file to a .gif with the same name
+#
+# Example Usages:
+#    togif file.mov
+# **************************************************************
+
+togif() {
+    [[ -z "$1" ]] && echo "$0: missing argument"
+    SIZE=$(ffprobe -v quiet -print_format json -show_streams $1 | jq '.streams[0] | "\(.width)x\(.height)"')
+    SIZE=$(sed -e 's/^"//' -e 's/"$//' <<< $SIZE)  # Remove leading and trailing quotes
+    ffmpeg -i $1 -s $SIZE -pix_fmt rgb24 -r 5 -f gif - | gifsicle --optimize=3 --delay=40 > ${1%.*}.gif
+}
+
+# **************************************************************
+# Converts a Quicktime .mov file to a .mp4 with the same name
+#
+# Example Usages:
+#    tomp4 file.mov
+# **************************************************************
+
+tomp4() {
+    [[ -z "$1" ]] && echo "$0: missing argument"
+    ffmpeg -i $1 -vcodec h264 -acodec aac -strict -2 ${1%.*}.mp4
+}
+
+# Acts just like cd but if env/bin/activate exists (python virtual environment)
+# then it will automatically be activated
+cdenv(){
+    'cd' $@
+    if [ -e "env/bin/activate" ]
+    then
+        source env/bin/activate
+    fi
+}
+
+# ================================================================================
+# GIT
+#
+# Functions meant to simplify and combine common git operations
+# ================================================================================
+
+amend() {
+    git add $@
+    git commit --amend
+}
+
+revert() {
+    HASH=$(git rev-parse --short $1)
+    echo "Creating branch revert-$HASH"
+    git checkout master
+    git pull
+    git checkout -b revert-$HASH
+    git revert $HASH
+    fab test
+}
+
+pushbranch() {
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    git push origin $BRANCH
+}
+
+pullbranch() {
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    git pull origin $BRANCH
+}
+
+
